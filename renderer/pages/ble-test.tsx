3b69c4a3df94be4976f1cc2883869aedb8d7c1dc
jungsoo.kim <jungsoo.kim@medicalpark.co.kr>
2025-09-22 13:44:04 +0900

    test app updated

--- renderer/pages/ble-test.tsx
+++ renderer/pages/ble-test.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { useBLE } from '../contexts/BLEContext'
 
-export default function BLETestPage() {
+export default function BleTest() {
   const {
     bleState,
     logs,
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,36 +1,10 @@
 import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
+import BleTest from "./ble-test";
 
 export default function HomePage() {
   return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-tailwindcss)</title>
-      </Head>
-      <div className="grid grid-col-1 text-2xl w-full text-center">
-        <div>
-          <Image
-            className="ml-auto mr-auto"
-            src="/images/logo.png"
-            alt="Logo image"
-            width={256}
-            height={256}
-          />
-        </div>
-        <span>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
-      </div>
-      <div className="mt-1 w-full flex-wrap flex justify-center space-x-4">
-        <Link href="/next" className="text-blue-600 hover:text-blue-800 underline">Go to next page</Link>
-        <Link href="/ble-test" className="text-green-600 hover:text-green-800 underline">BLE Test Dashboard</Link>
-      </div>
-    </React.Fragment>
+
+    <BleTest />
+
   )
 }
